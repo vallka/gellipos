768b4d33beafad8ffbe27f048471b03e1699ee7c
vallka <vallka@vallka.com.com>
2021-09-24 19:21:56 +0100

    ..

--- pos/admin.py
+++ pos/admin.py
@@ -6,18 +6,29 @@ from .models import *
 #admin.site.register(Product)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id_product','reference','name','ean13','quantity',]
+    list_display = ['id_product','name','reference','barcode_tag','quantity','sold']
+    list_display_links = list_display
     search_fields = ['id_product','reference','name','ean13', ]
-
+    readonly_fields = ['barcode_tag','sold']
 
 class DetailInline(admin.TabularInline):
     model = OrderDetail
     extra = 3
+
+    def has_add_permission(self,request, obj):
+        return not obj or obj._loaded_values['status']==Order.Status.NEW
+
+    def has_delete_permission(self,request, obj):
+        return not obj or obj._loaded_values['status']==Order.Status.NEW
+
+    def has_change_permission(self,request, obj):
+        return not obj or obj._loaded_values['status']==Order.Status.NEW
 
 #admin.site.register(Order)
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     inlines = [DetailInline]
     list_display = ['id','customer_name','customer_email','total','status',]
+    list_display_links = list_display
     search_fields = ['id','customer_name','customer_email','total' ]
     list_filter = ['status']
--- pos/models.py
+++ pos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import mark_safe
 
 # Create your models here.
 class Product(models.Model):
@@ -9,12 +10,38 @@ class Product(models.Model):
     ean13 = models.TextField('ean13',blank=True,default='')
     quantity = models.IntegerField('quantity',default=0)
 
+    def name_reference(self):
+        return mark_safe(self.name+"<br>"+self.reference)
+
+    def ean13_main(self):
+        return self.ean13[0:13]
+
+    def barcode_tag(self):
+        return mark_safe(f"<svg id='barcode_{self.id_product}'></svg>" +
+                "<script>"+
+                f"JsBarcode('#barcode_{self.id_product}', '{self.ean13_main()}',"+
+                "{height: 30,width: 2,fontSize: 12,font: 'Arial','textMargin':0,'margin':1,'flat':true});"+
+                "</script>"
+        )
+
+    def sold(self):
+        qnt = 0
+        prods = OrderDetail.objects.filter(product=self)
+        for prod in prods:
+            print(prod.order,prod.order.status,prod.quantity)
+            if prod.order.status==Order.Status.PAID:
+                qnt += prod.quantity
+        
+        return qnt
+
+
+
     def __str__(self):
         return self.reference + ' | £'+ str(self.price)+ ' | ' + self.ean13
 
 class Order(models.Model):
-    customer_name = models.CharField('customer_name',max_length=100)
-    customer_email = models.CharField('customer_email',max_length=100)
+    customer_name = models.CharField('customer_name',max_length=100,default='-')
+    customer_email = models.CharField('customer_email',max_length=100,default='-')
     created_dt = models.DateTimeField('created_dt',auto_now_add=True, null=True)
     updated_dt = models.DateTimeField('created_dt',auto_now=True, null=True)
     subtotal = models.DecimalField('subtotal',max_digits=8, decimal_places=2,default=0)
@@ -40,8 +67,27 @@ class Order(models.Model):
 
     status = models.IntegerField('Status',default=Status.NEW,choices=Status.choices)
 
+    @classmethod
+    def from_db(cls, db, field_names, values):
+        instance = super().from_db(db, field_names, values)
+        
+        # save original values, when model is loaded from database,
+        # in a separate attribute on the model
+        instance._loaded_values = dict(zip(field_names, values))
+        
+        return instance
+
+    def save(self, *args, **kwargs):
+        need_recalc = False
+        if not self._state.adding:
+            if self.status==Order.Status.PAID and (self._state.adding or self._loaded_values['status']==Order.Status.NEW):
+                # do something
+                need_recalc = True
+
+        super().save(*args, **kwargs)        
+
 class OrderDetail(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    order = models.ForeignKey(Order,on_delete=models.CASCADE,related_name='orderdetail_set')
     product = models.ForeignKey(Product,on_delete=models.DO_NOTHING)
     quantity = models.IntegerField('quantity',default=1)
 
